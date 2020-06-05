a673551adece7a567ffc22eec4530da5f346f9be
TMax66 <massimo.tranquillo@gmail.com>
2020-06-05 08:46:07 +0200

    first UP

---
+++ code..r
@@ -0,0 +1,42 @@
+library(readxl)
+library(tidyverse)
+library(knitr)
+library(kableExtra)
+ 
+
+dt<-dati %>% 
+  filter(mp=="MP00/013") %>% 
+  #select_if(~ !any(is.na(.))) %>% 
+  discard(~all(is.na(.x))) %>% 
+  mutate(s = rowSums(select(., starts_with("r")))) %>%
+  mutate(k=n()) %>% 
+  mutate(pi=s/k)
+
+
+repliche<-ncol(select(dt, starts_with("r")))
+pi<-dt$pi
+k<-dt$k
+P<-mean(pi)
+
+sigma2_r<-sum(pi*(1-pi))/k
+sigma2_L<-(sum((pi-P)^2))/(k-1)
+
+sigma2_R=sigma2_r+sigma2_L
+
+sd_r<-sqrt(sigma2_r)
+sd_R<-sqrt(sigma2_R)
+
+  
+tab<-round(tibble("n.Lab"=k,"n.rep"=repliche, "P"=P, sigma2_r,sigma2_L,sigma2_R,sd_r,sd_R, "r"=2*sd_r,
+       "R"=2*sd_R),2) %>% 
+  slice(1) %>% 
+  kable() %>% 
+  kable_styling()
+   
+
+
+  
+  
+
+
+
